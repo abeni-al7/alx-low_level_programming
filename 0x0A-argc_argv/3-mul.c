982b529a374900ffa172d2f8372d7e72dd162feb
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-02 17:32:28 +0000

    Multiply command line arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int a;
+	int b;
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
+	return (0);
+}
