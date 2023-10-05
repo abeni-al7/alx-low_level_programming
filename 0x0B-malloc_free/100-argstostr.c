afcf3c9fe8d77b842629848922b58b4b06b068bb
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-05 09:14:08 +0000

    Allocate the right amount of memory

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -20,7 +20,7 @@ char *argstostr(int ac, char **av)
 		len += strlen(av[i]) + 1;
 	}
 
-	str = malloc(len * sizeof(char));
+	str = malloc(len * sizeof(char) + 1);
 	if (str == NULL)
 		return (NULL);
 
