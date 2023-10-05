6ed9cb7c2462153a378f6b47daa9b59f52800269
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-05 09:21:19 +0000

    Handle failed case

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,7 +12,7 @@ char **strtow(char *str)
 	char **words;
 	char *temp;
 
-	if (str == NULL || strcmp(str, "") == 0)
+	if (str == NULL || strcmp(str, "") == 0, strcmp(str, " "))
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
