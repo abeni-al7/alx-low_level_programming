1f207067b694aa9549fa7de5b6e4ae37741e71d7
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-06 12:06:55 +0000

    Handle NULL for string concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,13 +13,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j = 0;
 	char *str;
 
-	if (n >= strlen(s2))
-		n = strlen(s2);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	str = malloc((strlen(s1) + n + 1 + 1) * sizeof(char));
+	if (n >= strlen(s2))
+		n = strlen(s2);
+	str = malloc((strlen(s1) + n + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
