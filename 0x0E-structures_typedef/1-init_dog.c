a800c3666f2d5420f22928f61d8089bc0304fd69
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-09 12:29:20 +0000

    Allocate memory when initializeing structure members

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	strcpy(d->name, name);
+	d->name = malloc(strlen(name) + 1);
+	if (d->name != NULL)
+		strcpy(d->name, name);
+
 	d->age = age;
-	strcpy(d->owner, owner);
+
+	d->owner = malloc(strlen(owner) + 1);
+	if (d->owner != NULL)
+		strcpy(d->owner, owner);
 }
