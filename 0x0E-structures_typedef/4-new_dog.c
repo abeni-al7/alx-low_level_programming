ef84e662618562e9837709060264e4e785beb02d
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-09 13:49:37 +0000

    Dynamically allocate memory for the created struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,7 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = name;
+	strcpy(new_dog->name, name);
 	new_dog->age = age;
 	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
@@ -30,6 +30,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->owner = owner;
+	strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
