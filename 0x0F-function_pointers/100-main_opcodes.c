05609dc5de9380949c965b444414b0e71d3d8010
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-11 17:34:30 +0000

    Create a program that prints opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+
+void print_opcodes(int num_bytes);
+
+/**
+ * main - entry point
+ * @ac: argument count
+ * @av: array of arguments
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	unsigned char *start;
+	size_t bytes;
+	int size, i = 0;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	size = atoi(av[1]);
+	if (size < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	bytes = (size_t)size;
+	start = (unsigned char *)&main;
+	for (i = 0; i < size; i++)
+		printf("%02x ", start[i]);
+	printf("\n");
+
+	return (0);
+}	
