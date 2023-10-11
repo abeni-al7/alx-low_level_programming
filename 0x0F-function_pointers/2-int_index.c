395f4809c43094170b6a80dad9e5bf163e6fc392
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-11 17:00:03 +0000

    Handle null for the int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0)
 		return (-1);
 	while (i < size)
 	{
-		if (cmp(array[i]) == 1)
+		if (cmp(array[i]) != 0)
 			return (i);
 		i++;
 	}
