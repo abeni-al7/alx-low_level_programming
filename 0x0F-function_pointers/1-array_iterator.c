b667bd05b650b90c9f988d7a22eedb3d4e1a05a3
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-11 16:56:08 +0000

    Handle null for array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes a function on each element of an array
  * @array: array
  * @size: size of the array
  * @action: pointer to the function to be called
@@ -11,9 +11,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	while (i < size)
+	if (array != NULL && action != NULL)
 	{
-		action(array[i]);
-		i++;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 	}
 }
