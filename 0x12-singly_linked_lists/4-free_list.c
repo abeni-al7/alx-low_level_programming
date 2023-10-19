63ff13bdf6fe8270b74aa040a5f1f8e517fcc39f
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-19 16:37:40 +0000

    Handle freeing a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,10 @@ void free_list(list_t *head)
 	list_t *current = head;
 	list_t *next_node;
 
-	while (current->next != NULL)
+	while (current != NULL)
 	{
 		next_node = current->next;
 		free(current);
 		current = next_node;
 	}
-
-	free(current);
 }
