1a0e9810b167270e69297a3464a90c5f6347af0f
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-19 15:07:17 +0000

    Handle number of elements of a singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a linked list
+ * @h: pointer to first element of linked list
+ * Return: number of elements in linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
