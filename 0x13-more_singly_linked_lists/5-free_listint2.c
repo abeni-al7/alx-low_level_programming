d7b7b01524d3b0bfb47bf369b35225a65408963d
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-24 10:37:41 +0000

    Fix freeing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+	listint_t *current = *head;
+	listint_t *next;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return;
-	while (*head != NULL)
+	while (current != NULL)
 	{
-		current = *head;
-		*head = current->next;
+		next = current->next;
 		free(current);
+		current = next;
 	}
 	*head = NULL;
 }
