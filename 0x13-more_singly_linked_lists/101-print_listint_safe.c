e90aac0f6d1e4a0aa0406f63209793954df08a90
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-24 13:11:24 +0000

    print a linked list with address of each element

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list with the address
+ * @head: pointer to the first element of the list
+ * Return: number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num = 0;
+	const listint_t *visited[100];
+	size_t i;
+
+	while (head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (head == visited[i])
+				return (num);
+		}
+		visited[num] = head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		num++;
+	}
+
+	return (num);
+}
