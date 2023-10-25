49fc9dd922476f24c73a50e533d1c6a6abaf6ad2
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-25 10:19:41 +0000

    Print loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,74 +1,6 @@
 #include "lists.h"
 
 /**
- * print_list - print a linked list
- * @head: pointer to the first element of the list
- * Return: number of nodes printed
- */
-
-size_t print_list(const listint_t *head)
-{
-	size_t num = 0;
-	const listint_t *current;
-
-	current = head;
-	while (current != NULL)
-	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		num++;
-		current = current->next;
-	}
-
-	return (num);
-}
-
-/**
- * print_list_in_loop - print the list if there is a loop
- * @head: pointer to the first element of the list
- * @loop: address of the loop
- * Return: number of nodes printed
- */
-
-size_t print_list_in_loop(const listint_t *head, const listint_t *loop)
-{
-	size_t num = 0;
-	const listint_t *current;
-
-	current = head;
-	while (current != loop)
-	{
-		printf("[%p] %d\n", (const void *)current, current->n);
-		num++;
-		current = current->next;
-	}
-	printf("-> [%p] %d\n", (void *)current, current->n);
-	num++;
-
-	return (num);
-}
-
-/**
- * find_loop_start - finds the start of a loop in a linked list
- * @head: pointer to the first element of linked list
- * @meet: meeting point of head with the loop
- * Return: the location of the loop
- */
-
-const listint_t *find_loop_start(const listint_t *head, const listint_t *meet)
-{
-	const listint_t *start;
-
-	start = head;
-	while (start != meet)
-	{
-		start = start->next;
-		meet = meet->next;
-	}
-
-	return (start);
-}
-
-/**
  * print_listint_safe - prints a linked list with the address
  * @head: pointer to the first element of the list
  * Return: number of nodes in the list
@@ -77,27 +9,36 @@ const listint_t *find_loop_start(const listint_t *head, const listint_t *meet)
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
-	const listint_t *slow, *fast, *loop;
+	const listint_t *slow, *fast;
 
-	if (head == NULL)
-		exit(98);
+	slow = head;
+	fast = head;
 
-	slow = fast = head;
 	while (slow && fast && fast->next)
 	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		num++;
+
 		slow = slow->next;
 		fast = fast->next->next;
 
 		if (slow == fast)
 		{
-			loop = find_loop_start(head, slow);
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			num++;
 			break;
 		}
 	}
-	if (loop)
-		num = print_list_in_loop(head, loop);
-	else
-		num = print_list(head);
+
+	if (!slow || !fast || !fast->next)
+	{
+		while (head)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			num++;
+			head = head->next;
+		}
+	}
 
 	return (num);
 }
