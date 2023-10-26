ec298a1e134d4a0abc7a1adfc7b0bbdfbb81ed34
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-26 17:18:16 +0000

    Fix the get bit function

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,14 +11,15 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
 	while (i < index)
 	{
 		n >>= 1;
 		i++;
 	}
-	if (n == 0)
-		return (-1);
-	else if (n & 1)
+	if (n & 1)
 		return (1);
 	else
 		return (0);
