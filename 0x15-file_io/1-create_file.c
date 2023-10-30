436b0dc5556d8a922f13299242a13b164afd5825
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-30 18:03:59 +0000

    Handled partial writing error in create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	mode_t original = umask(0);
 	ssize_t written;
 	int fd;
-	size_t text_len = strlen(text_content);
+	ssize_t text_len = strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
