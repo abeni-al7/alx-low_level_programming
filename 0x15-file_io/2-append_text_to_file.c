f73d68adf77f4d0d925e86de9bb418ea20c459d6
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-30 18:37:43 +0000

    Fix the append function

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,16 +18,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (1);
-	}
-	written = write(fd, text_content, text_len);
-	if (written == -1 || written != text_len)
-	{
-		close(fd);
-		return (-1);
+		written = write(fd, text_content, text_len);
+		if (written == -1 || written != text_len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
