5dcbbcdd69c8189f7eabf91a9933f8e775642a70
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-30 17:52:31 +0000

    Handled original permission and adding no text in create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,20 +9,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	mode_t permission = 0600;
+	mode_t original = umask(0);
 	ssize_t written;
 	int fd;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permission);
 	if (fd == -1)
 		return (-1);
-	written = write(fd, text_content, sizeof(text_content));
-	if (written == -1)
+	umask(original);
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		written = write(fd, text_content, sizeof(text_content));
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
