48ea3cd109a32ee9b98e86bc9ed2eb6d8ec8046d
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-30 19:12:21 +0000

    Improve the copy function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * handle_error - handles error
+ * @message: error message
+ * @args: argument for formatted message
+ * @code: error code
+ */
+
+void handle_error(int code, const char *message, const char *args)
+{
+	dprintf(STDERR_FILENO, message, args);
+	exit(code);
+}
+
+/**
  * main - copy the contents of a file to another file
  * @ac: argument count
  * @av: array of arguments
@@ -10,47 +23,29 @@
 int main(int ac, char **av)
 {
 	char buffer[1024];
-	ssize_t nb_read, written;
+	ssize_t nb_read = -1, written;
 	mode_t permission = 0664;
 	mode_t original = umask(0);
 	int fd_from, fd_to;
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
-		exit(97);
-	}
-
+		handle_error(97, "Usage: %s file_from file_to\n", av[0]);
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[0]);
-		exit(98);
-	}
-
+		handle_error(98, "Error: Can't read from file %s\n", av[1]);
 	fd_to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, permission);
 	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		handle_error(99, "Error: Can't write to %s\n", av[2]);
 	umask(original);
 	while (nb_read != 0)
 	{
 		nb_read = read(fd_from, buffer, sizeof(buffer));
 		if (nb_read == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", av[0]);
-			exit(98);
-		}
+			handle_error(98, "Error: Can't read from file %s\n", av[1]);
 		written = write(fd_to, buffer, nb_read);
 		if (written == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+			handle_error(99, "Error: Can't write to %s\n", av[2]);
 	}
-
 	if (close(fd_from) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_from);
@@ -61,6 +56,5 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return (0);
 }
