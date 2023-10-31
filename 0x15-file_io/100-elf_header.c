9dd70b25db9ef11d80000d8b18cbc9b4bdf7279d
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-31 11:38:19 +0000

    Read elf files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -13,6 +13,38 @@ void handle_error(int code, const char *message)
 }
 
 /**
+ * print_elf - prints the elf header
+ * @header: header to print
+ */
+
+void print_elf(const Elf64_Ehdr *header)
+{
+	int i;
+
+	printf("ELF Header:\n");
+	printf("	Magic: ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf(" %02x", header->e_ident[i]);
+	printf("\n");
+	printf("	Class:	%s\n", 
+		header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("	Data:	%s\n",
+		header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf("	Version:	%d(current)\n",
+		header->e_ident[EI_VERSION]);
+	printf("	OS/ABI:	%s\n",
+		header->e_ident[EI_OSABI]);
+	printf("	ABI Version:	%d\n",
+		header->e_ident[EI_ABIVERSION]);
+	printf("	Type:	%s\n",
+		header->e_type == ET_EXEC ? "Advanced Micro Devices X86-64" : "Unknown");
+	printf("	Version:	0x%x\n",
+		header->e_version);
+	printf("	Entry point address:	0x%lx\n",
+		header->e_entry);
+}
+
+/**
  * main - read an elf file
  * @ac: argument count
  * @av: array of arguments
@@ -24,7 +56,8 @@ int main(int ac, char **av)
 	unsigned int i;
 	unsigned char magic[] = {0x7F, 'E', 'L', 'F'};
 	int fd;
-	char *filename, buffer[62];
+	char *filename;
+	Elf64_Ehdr buffer;
 
 	if (ac != 2)
 		handle_error(98, "Usage: elf_header elf_filename\n");
@@ -35,13 +68,11 @@ int main(int ac, char **av)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		handle_error(98, "Can't open file\n");
-	if (read(fd, buffer, sizeof(buffer)) == -1)
+	if (read(fd, &buffer, sizeof(buffer)) == -1)
 		handle_error(98, "File can not be read\n");
-	if (memcmp(buffer, magic, 4) != 0)
+	if (memcmp(buffer.e_ident, ELFMAG, SELFMAG) != 0)
 		handle_error(98, "File is not ELF\n");
-	for (i = 0; i < sizeof(buffer); i++)
-		printf("%02x ", buffer[i]);
-	printf("\n");
+	print_elf(&buffer);
 
 	if (close(fd) == -1)
 		handle_error(98, "Can't close the elf file\n");
