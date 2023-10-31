9602a477cbf4d5fa76505017a7407e6b3108c2fe
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-31 12:14:11 +0000

    Read elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,78 +2,68 @@
 
 /**
  * handle_error - handles error
- * @code: exit code
- * @message: error message
+ * @error_code: error code
+ * @error_message: error message
  */
 
-void handle_error(int code, const char *message)
+void handle_error(int error_code, const char* error_message)
 {
-	dprintf(STDERR_FILENO, message, "");
-	exit(code);
+	fprintf(stderr, "Error %d: %s\n", error_code, error_message);
+	exit(98);
 }
 
 /**
- * print_elf - prints the elf header
- * @header: header to print
+ * print_elf_header - print the elf header
+ * @header: header
  */
 
-void print_elf(const Elf64_Ehdr *header)
+void print_elf_header(const Elf64_Ehdr *header)
 {
 	int i;
 
-	printf("ELF Header:\n");
-	printf("	Magic: ");
+	printf("Magic: ");
 	for (i = 0; i < EI_NIDENT; i++)
-		printf(" %02x", header->e_ident[i]);
+		printf("%02x ", header->e_ident[i]);
 	printf("\n");
-	printf("	Class:	%s\n",
-		header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("	Data:	%s\n",
-		header->e_ident[EI_DATA] == ELFDATA2LSB ?
-		"2's complement, little endian" : "2's complement, big endian");
-	printf("	Version:	%d(current)\n",
-		header->e_ident[EI_VERSION]);
-	printf("	OS/ABI:	%d\n",
-		header->e_ident[EI_OSABI]);
-	printf("	ABI Version:	%d\n",
-		header->e_ident[EI_ABIVERSION]);
-	printf("	Type:	%s\n",
-		header->e_type == ET_EXEC ? "Advanced Micro Devices X86-64" : "Unknown");
-	printf("	Version:	0x%x\n",
-		header->e_version);
-	printf("	Entry point address:	0x%lx\n",
-		header->e_entry);
+	printf("Class: %s\n", 
+		header->e_ident[EI_CLASS] == ELFCLASS32 ?
+		"ELF32" : (header->e_ident[EI_CLASS] == ELFCLASS64 ?
+		"ELF64" : "Unknown"));
+	printf("Data: %s\n", 
+	header->e_ident[EI_DATA] == ELFDATA2LSB ?
+	"2's complement, little endian" : (header->e_ident[EI_DATA] == ELFDATA2MSB ?
+	"2's complement, big endian" : "Unknown"));
+	printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
+	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("Type: %d\n", header->e_type);
+	printf("Entry point address: %lx\n", header->e_entry);
 }
 
 /**
- * main - read an elf file
- * @ac: argument count
- * @av: array of arguments
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of arguments
  * Return: 0
  */
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
+	char *filename = argv[1];
 	int fd;
-	char *filename;
-	Elf64_Ehdr buffer;
+	Elf64_Ehdr header;
 
-	if (ac != 2)
-		handle_error(98, "Usage: elf_header elf_filename\n");
-	if (av[1] == NULL)
-		handle_error(98, "File name is NULL\n");
-	filename = av[1];
-
+	if (argc != 2)
+		handle_error(98, "Usage: elf_header elf_filename");
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		handle_error(98, "Can't open file\n");
-	if (read(fd, &buffer, sizeof(buffer)) == -1)
-		handle_error(98, "File can not be read\n");
-	if (memcmp(buffer.e_ident, ELFMAG, SELFMAG) != 0)
-		handle_error(98, "File is not ELF\n");
-	print_elf(&buffer);
-
+		handle_error(98, "Can't open file");
+	if (read(fd, &header, sizeof(header)) == -1)
+		handle_error(98, "File can not be read");
+	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
+		handle_error(98, "File is not an ELF file");
+	print_elf_header(&header);
 	if (close(fd) == -1)
-		handle_error(98, "Can't close the elf file\n");
-	return (0);
+		handle_error(98, "Can't close the file");
+	return 0;
 }
