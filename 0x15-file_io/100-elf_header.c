9a04dd00165a83672aaf215869ed2c84c1c8100a
abeni-al7 <abenezeralebachew3@gmail.com>
2023-10-31 11:50:27 +0000

    Read elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -26,10 +26,11 @@ void print_elf(const Elf64_Ehdr *header)
 	for (i = 0; i < EI_NIDENT; i++)
 		printf(" %02x", header->e_ident[i]);
 	printf("\n");
-	printf("	Class:	%s\n", 
+	printf("	Class:	%s\n",
 		header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
 	printf("	Data:	%s\n",
-		header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+		header->e_ident[EI_DATA] == ELFDATA2LSB ? 
+		"2's complement, little endian" : "2's complement, big endian");
 	printf("	Version:	%d(current)\n",
 		header->e_ident[EI_VERSION]);
 	printf("	OS/ABI:	%d\n",
