789ae7a67e4f976ced041160bec15b761116650d
abeni-al7 <abenezeralebachew3@gmail.com>
2023-12-22 20:07:05 +0000

    Print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+			if (current != NULL)
+				printf(", ");
+		}
+		if (i < ht->size - 1 && ht->array[i + 1] != NULL)
+			printf(", ");
+	}
+	printf("}\n");
+}
