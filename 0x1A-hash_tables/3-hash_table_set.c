4eeb25b47877f6d9fc0166a641906e951ff5427e
abeni-al7 <abenezeralebachew3@gmail.com>
2023-12-22 19:19:42 +0000

    Add a node to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,6 +48,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new_node->next = ht->array[index];
-	ht->array[index] = new_node;i
+	ht->array[index] = new_node;
 	return (1);
 }
