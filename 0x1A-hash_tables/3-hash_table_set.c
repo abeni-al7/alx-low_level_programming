e7ccf47de74cbb3a7fc646cf6ffbac33bd4a0a36
abeni-al7 <abenezeralebachew3@gmail.com>
2023-12-22 18:43:44 +0000

    Add a node to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,29 +10,45 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *current = NULL, *new_node = NULL;
+	hash_node_t *current, *new_node;
 	unsigned long int index;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
+
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
 			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
 			return (1);
 		}
 		current = current->next;
 	}
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
