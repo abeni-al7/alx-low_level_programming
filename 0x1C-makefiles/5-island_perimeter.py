0f69418b9c9574a5915f6c88ccca5e2689e28e8f
abeni-al7 <abenezeralebachew3@gmail.com>
2024-02-29 14:30:04 +0300

    Island perm

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Island perimeter"""
+
+def island_perimeter(grid):
+	visit = set()
+	def depth_first_search(i, j):
+		if i >= len(grid) or j >= len(grid[0] or i < 0 or j < 0 or grid[i][j] == 0:
+			return 1
+		if (i, j) in visit:
+			return 0
+		visit.add((i, j))
+		perimeter = depth_first_search(i, j)
+		perimeter += depth_first_search(i, j+1)
+		perimeter += depth_first_search(i, j-1)
+		perimeter += depth_first_search(i+1, j)
+		perimeter += depth_first_search(i-1, j)
+		return perimeter
+	for i in range(len(grid)):
+		for j in range(len(grid[0])):
+			if grid[i][j] == 1:
+				depth_first_search(i, j)
