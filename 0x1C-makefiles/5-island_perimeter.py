8d4e42bcf06c45f3d95d9da5a759e0a523837732
abeni-al7 <abenezeralebachew3@gmail.com>
2024-02-29 14:51:48 +0300

    Fix island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,4 +21,8 @@ def island_perimeter(grid):
 	for i in range(len(grid)):
 		for j in range(len(grid[0])):
 			if grid[i][j] == 1:
-				return depth_first_search(i, j)
+				if depth_first_search(i, i) != None:
+					return depth_first_search(i, j)
+	return 0
+
+print(island_perimeter([[0], [0], [0]]))
